7a83b25e0055fa9c073a330caf1ea80f9c7b9dfc
Squeeks <privacymyass@gmail.com>
2011-11-18 10:03:03 +0000

    OH HAI, MORE DOX

--- model/quotedb.py
+++ model/quotedb.py
@@ -1,5 +1,15 @@
 from google.appengine.ext import db
 
+# _________________________________________ 
+#/ <eggsby> I know it insulted his         \
+#\ intelligence but his code insulted mine /
+# ----------------------------------------- 
+#        \   ^__^
+#         \  (oo)\_______
+#            (__)\       )\/\
+#                ||----w |
+#                ||     ||
+
 class QuoteDB(db.Model):
     quoteMD5    = db.StringProperty()
     quoteString = db.StringProperty(multiline=True)
