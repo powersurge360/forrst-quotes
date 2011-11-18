51c1b0fa1c0b372c46e5c2709fde74a2f3fdb309
Yorick Peterse <yorickpeterse@gmail.com>
2011-11-18 13:24:05 +0100

    Show quotes on the frontpage.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- controller/base.py
+++ controller/base.py
@@ -3,7 +3,12 @@ from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 
 class Base(webapp.RequestHandler):
+    """
+        Base controller that makes it easier to render views.
+    """
+
     view_path = "./view/{0}.html"
+
     def _render_view(self, view, data = {}):
         """
             Renders the given view along with the given data and sends it to the
@@ -14,9 +19,9 @@ class Base(webapp.RequestHandler):
                   and without an extension.
                 - data: A dictionary with keys and values to insert into the
                   template.
-        """
 
-        #view   = template.render(os.path.join('./view', view + '.html'), data)
-        #layout = os.path.join('./layout/default.html')
+            Example:
+                self._render_view('index', {'quotes': [....]})
+        """
 
         self.response.out.write(template.render(self.view_path.format(view), data))
--- controller/mainhandler.py
+++ controller/mainhandler.py
@@ -1,9 +1,15 @@
 from base import *
+import model
 
 class MainHandler(Base):
+    """
+        Controller that handles the frontpage.
+    """
+
     def get(self):
         """
             GET - Shows the homepage containing a list of quotes.
         """
 
-        self._render_view('index')
+        quotes = model.QuoteDB.all().order('-created')
+        self._render_view('index', {'quotes': quotes})
